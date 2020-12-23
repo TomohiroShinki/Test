d5e8d142f8fd041ad666da282e689bfa440d4297
Teisyoku sann <xboxtomohiro@gmail.com>
2020-12-23 14:01:03 +0900

    Waht

---
+++ github/test/PIL_test/label_png.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+import tkinter as Tk
+from PIL import Image, ImageTk
+
+CLOCK = 'meza-bl-2'
+
+class Frame(Tk.Frame):
+    def __init__(self, master=None):
+        Tk.Frame.__init__(self, master)
+        image = Image.open(CLOCK)
+        self.img = ImageTk.PhotoImage(image)
+
+
+        il = Tk.Label(self, image=self.img)
+        il.pack()
+
+
+if __name__ == '__main__':
+    f = Frame()
+    f.pack()
+    f.mainloop()
